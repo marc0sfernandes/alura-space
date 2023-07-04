8fa0474701107736decaacc208a4af1624f110e3
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-07-04 10:40:29 -0300

    create paginas and componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import PaginaInicial from "./paginas/PaginaInicial";
+import './styles/estilosGlobais.scss';
 
 export default function App() {
   return (
-    <h1>Alura Space</h1>
+    <PaginaInicial/>
   );
 }
