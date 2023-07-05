46c67d2220b418a3e39d6cadeb68626316257f64
marc0sfernades <marcosvini1979.mv@gmail.com>
2023-07-05 08:37:42 -0300

    create component galeria

---
+++ src/componentes/Tags/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Tags() {
+  return (
+    <div>
+        <p>Filtre por Tags:</p>
+        <ul>
+            <li>Estrelas</li>
+            <li>Galáxias</li>
+            <li>Lua</li>
+            <li>Planetas</li>
+        </ul>
+    </div>
+  )
+}
